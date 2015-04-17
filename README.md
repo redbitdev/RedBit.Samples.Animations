c55075ac086a0d90a3d25a1cf2abc39a62a12f31
Mark Arteaga <mark@redbitdev.com>
2015-04-17 14:06:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RedBit.Samples.XForms.Animations
+Sample code for cross platform animationsusing Xamarin Forms for iOS, Android and Windows
---
+++ src/RedBit.Animations.XForms.Animation/RedBit.Animations.XForms.Animation.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RedBit.Animations.XForms.Animation.WinPhone.Resources;
+
+namespace RedBit.Animations.XForms.Animation.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
